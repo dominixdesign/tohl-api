24d35df7240494f60f38191ab18504ea39223e62
Dominik Sander <dominik.sander@googlemail.com>
2021-04-07 20:49:13 +0200

    add possibility to run only a single updater

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 const updater = require('./updater')
 const sqlite3 = require('sqlite3').verbose()
+const fs = require('fs')
+let runOnly
+
+try {
+  if (fs.existsSync('./.run-only')) {
+    runOnly = fs
+      .readFileSync('./.run-only', { encoding: 'utf8', flag: 'r' })
+      .trim()
+    if (!Object.keys(updater).includes(runOnly)) {
+      runOnly = undefined
+    }
+  }
+} catch (err) {
+  // error
+}
 
 const db = new sqlite3.Database(':memory:')
 
@@ -7,7 +22,9 @@ const { initDatabase } = require('./db/init')
 initDatabase(db)
 
 Object.keys(updater).map((update) => {
-  updater[update].run(db)
+  if (!runOnly || (runOnly && runOnly === update)) {
+    updater[update].run(db)
+  }
 })
 
 //db.each('SELECT rowid AS id, sim_name FROM teams', function (err, row) {
