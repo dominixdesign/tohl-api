02e337d0689ce262280e4014028a1e80eaab2aa9
Dominik Sander <dominik.sander@googlemail.com>
2021-05-06 21:05:15 +0200

    restart test

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  return res.send('Received a GET HTTP method')
+  return res.send('Received a GET HTTP method after the restart')
 })
 
 app.post('/', (req, res) => {
