dadf48b5836eeaaeb8a198660b74ee62e77ab9a3
Dominik Sander <dominik.sander@googlemail.com>
2021-06-13 23:09:27 +0200

    sum stats

--- updater/player/stats.js
+++ updater/player/stats.js
@@ -7,6 +7,25 @@ const log = require('../../server/helpers/logger')
 module.exports = {
   run: async () => {
     const season = detectSeason()
-    log(`### ${season} ### START ### PLAYER STATS ###`)
+    log(`### ${season} ### STRT ### PLAYER STATS ###`)
+
+    const allLineupArray = await db('lineup')
+      .where('season', season)
+      .select('player')
+      .sum('goals as sum_goals')
+      .sum('assists as sum_assists')
+      .sum('points as sum_points')
+      .sum('plusminus as sum_plusminus')
+      .sum('pim as sum_pim')
+      .sum('shots as sum_shots')
+      .sum('hits as sum_hits')
+      .sum('icetime as sum_icetime')
+      .groupBy('player', 'team')
+
+    for (const statsRow of allLineupArray) {
+      console.log(statsRow)
+    }
+
+    log(`### ${season} ### DONE ### PLAYER STATS ###`)
   }
 }
