944dc8e9a6311911c23ede341aaa37e851df8e8b
Dominik Sander <dominik.sander@googlemail.com>
2021-06-14 22:53:17 +0200

    add summed stats

--- updater/player/stats.js
+++ updater/player/stats.js
@@ -1,5 +1,3 @@
-const loadFHLFile = require('../../lib/filesystem/loadFHLFile')
-const generatePlayerId = require('../../lib/playerId')
 const detectSeason = require('../../lib/detectSeason')
 const db = require('../../server/helpers/db')
 const log = require('../../server/helpers/logger')
@@ -9,22 +7,42 @@ module.exports = {
     const season = detectSeason()
     log(`### ${season} ### STRT ### PLAYER STATS ###`)
 
+    // clean playerstats for this season
+
     const allLineupArray = await db('lineup')
       .where('season', season)
-      .select('player')
-      .sum('goals as sum_goals')
-      .sum('assists as sum_assists')
-      .sum('points as sum_points')
-      .sum('plusminus as sum_plusminus')
-      .sum('pim as sum_pim')
-      .sum('shots as sum_shots')
-      .sum('hits as sum_hits')
-      .sum('icetime as sum_icetime')
+      .select('player', 'team', 'season')
+      .count('points as games')
+      .sum('goals as goals')
+      .sum('assists as assists')
+      .sum('points as points')
+      .sum('plusminus as plusminus')
+      .sum('pim as pim')
+      .sum('shots as shots')
+      .sum('hits as hits')
+      .sum('icetime as icetime')
+      .sum('fightswon as fightswon')
+      .sum('fightslose as fightslose')
+      .sum('fightsdraw as fightsdraw')
+      .sum({
+        first_stars: db.raw("case when star like 'first' then 1 else 0 end"),
+        second_stars: db.raw("case when star like 'second' then 1 else 0 end"),
+        third_stars: db.raw("case when star like 'third' then 1 else 0 end")
+      })
+      .sum('minutes as minutes')
+      .sum('saves as saves')
+      .sum('shotsfaced as shotsfaced')
+      .sum('goalsagainst as goalsagainst')
+      .count('ejected as ejections')
+      .count('injured as injuries')
       .groupBy('player', 'team')
 
-    for (const statsRow of allLineupArray) {
-      console.log(statsRow)
-    }
+    await db('playerstats')
+      .insert(allLineupArray)
+      .onConflict()
+      .merge()
+      .then()
+      .catch()
 
     log(`### ${season} ### DONE ### PLAYER STATS ###`)
   }
