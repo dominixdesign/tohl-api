d8f11a6de16055944437d29e4cd9e70cb05cc6ee
Dominik Sander <dominik.sander@googlemail.com>
2021-06-16 23:06:48 +0200

    add first teamstats

--- updater/games/schedule.js
+++ updater/games/schedule.js
@@ -25,37 +25,125 @@ module.exports = {
     const season = detectSeason()
     log(`### ${season} ### STRT ### SCHEDULE ###`)
 
+    const defaultTeamstats = Object.freeze({
+      games: 0,
+      wins: 0,
+      loss: 0,
+      ties: 0,
+      points: 0,
+      goalsfor: 0,
+      goalsagainst: 0,
+      diff: 0,
+      winp: 0,
+      streak: ''
+    })
+
+    const updateStreak = (team, currentStreak, winner) => {
+      const curType = currentStreak.substr(0, 1)
+      const curVal = parseInt(currentStreak.substr(1))
+      if (team === winner) {
+        if (curType === 'W') {
+          return `W${curVal + 1}`
+        }
+        return 'W1'
+      }
+      if (team !== winner && winner !== null) {
+        if (curType === 'N') {
+          return `N${curVal + 1}`
+        }
+        return 'N1'
+      }
+      if (winner === null) {
+        if (curType === 'T') {
+          return `T${curVal + 1}`
+        }
+        return 'T1'
+      }
+    }
+
     let rawHtml = loadFHLFile('Schedule')
     let gameday = 1
     let gameInsert = []
+    let teamstats = {}
     for (const entry of rawHtml.split('<BR>')) {
       const gameData = gamePattern.exec(entry)
       gamePattern.lastIndex = 0
       if (gameData) {
         const { groups } = gameData
+        const home = team(groups.home)
+        const away = team(groups.away)
+        const overtimes = groups.ot ? 1 : null
+        let winner = null,
+          loser = null
+        if (!teamstats[home]) {
+          teamstats[home] = Object.assign({}, defaultTeamstats)
+        }
+        if (!teamstats[away]) {
+          teamstats[away] = Object.assign({}, defaultTeamstats)
+        }
         const goalshome = parseInt(groups.goalshome.trim())
         const goalsaway = parseInt(groups.goalsaway.trim())
+        teamstats[home].games++
+        teamstats[away].games++
+
+        teamstats[home].goalsfor += goalshome
+        teamstats[away].goalsfor += goalsaway
+        teamstats[home].goalsagainst += goalsaway
+        teamstats[away].goalsagainst += goalshome
+        teamstats[home].diff =
+          teamstats[home].goalsfor - teamstats[home].goalsagainst
+        teamstats[away].diff =
+          teamstats[away].goalsfor - teamstats[away].goalsagainst
+        if (goalshome > goalsaway) {
+          teamstats[home].wins++
+          teamstats[away].loss++
+          teamstats[home].points += 2
+          if (overtimes) {
+            teamstats[away].points++
+          }
+          winner = home
+          loser = away
+        } else if (goalshome < goalsaway) {
+          teamstats[away].wins++
+          teamstats[home].loss++
+          teamstats[away].points += 2
+          if (overtimes) {
+            teamstats[home].points++
+          }
+          winner = away
+          loser = home
+        } else if (goalshome === goalsaway) {
+          teamstats[away].ties++
+          teamstats[home].ties++
+          teamstats[away].points++
+          teamstats[home].points++
+        }
+        teamstats[home].streak = updateStreak(
+          home,
+          teamstats[home].streak,
+          winner
+        )
+        teamstats[away].streak = updateStreak(
+          away,
+          teamstats[away].streak,
+          winner
+        )
+        teamstats[home].winp =
+          teamstats[home].points / 2 / teamstats[home].games
+        teamstats[away].winp =
+          teamstats[away].points / 2 / teamstats[away].games
+
         gameInsert.push({
           season,
           game: groups.gamenumber,
           gameday,
-          home: team(groups.home),
-          away: team(groups.away),
+          home,
+          away,
           goalshome,
           goalsaway,
-          overtimes: groups.ot ? 1 : null,
-          winner:
-            goalshome > goalsaway
-              ? team(groups.home)
-              : goalsaway > goalshome
-              ? team(groups.away)
-              : null,
-          loser:
-            goalshome > goalsaway
-              ? team(groups.away)
-              : goalsaway > goalshome
-              ? team(groups.home)
-              : null
+          overtimes,
+          winner,
+          loser
         })
       } else {
         const dayData = gameDayPattern.exec(entry)
@@ -64,6 +152,7 @@ module.exports = {
         }
       }
     }
+    console.log(teamstats)
     if (gameInsert.length > 0) {
       await db('game')
         .insert(gameInsert)
