e1643cb2022e8f3304d83c15efa845e95d5502cd
Dominik Sander <dominik.sander@googlemail.com>
2021-06-20 12:14:11 +0200

    add division and conferences to teams

--- updater/teams.js
+++ updater/teams.js
@@ -4,29 +4,61 @@ const db = require('../server/helpers/db')
 
 module.exports = {
   run: () => {
+    const season = detectSeason()
     let rawHtml = loadFHLFile('TeamScoring')
+    let rawHtmlStandings = loadFHLFile('Standings')
+    const divisionAssignment = rawHtmlStandings.split('<H3>')
 
+    const teamsToDivision = {}
+    const teamsToConference = {}
+
+    for (const html of divisionAssignment) {
+      const divisionRegex = html.match(
+        /(?<divison>[A-Z]+) Division<\/H3>|(?<conference>[A-Z]+) Conference<\/H3>/
+      )
+      if (divisionRegex) {
+        const teamsMatches = [...html.matchAll(/#(?<team>[A-Z]+)/gm)] || []
+        for (const { groups } of teamsMatches) {
+          if (divisionRegex.groups.divison) {
+            teamsToDivision[groups.team.toLowerCase()] =
+              divisionRegex.groups.divison.toLowerCase()
+          }
+          if (divisionRegex.groups.conference) {
+            teamsToConference[groups.team.toLowerCase()] =
+              divisionRegex.groups.conference.toLowerCase()
+          }
+        }
+      }
+    }
+
+    const teamInserts = []
     const teams = rawHtml.split('<H2>')
-    teams.map((html) => {
+    for (const html of teams) {
       const teamnameRegex = html.match(/>([A-Z]{1,20})</)
       let teamname
       if (teamnameRegex) {
         teamname = teamnameRegex[1]
         const teamIdRegex = html.match(`${teamname} Totals +(?<id>[A-Z]{2,3})`)
         if (teamIdRegex) {
-          // insert to db
-          db('team')
-            .insert({
-              teamid: teamIdRegex.groups.id.toLowerCase(),
-              season: detectSeason(),
-              teamsim: teamname.toLowerCase()
-            })
-            .onConflict()
-            .merge(['teamsim'])
-            .then()
-            .catch((e) => console.log(e))
+          const teamid = teamIdRegex.groups.id.toLowerCase()
+          teamInserts.push({
+            teamid,
+            season,
+            conference: teamsToConference[teamname.toLowerCase()],
+            division: teamsToDivision[teamname.toLowerCase()],
+            teamsim: teamname.toLowerCase()
+          })
         }
       }
-    })
+    }
+
+    if (teamInserts.length > 0) {
+      db('team')
+        .insert(teamInserts)
+        .onConflict()
+        .merge(['teamsim', 'conference', 'division'])
+        .then()
+        .catch((e) => console.log(e))
+    }
   }
 }
