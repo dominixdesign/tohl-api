5eb82a39120ad8ca1937e25aaf12a534879b0f80
Dominik Sander <dominik.sander@googlemail.com>
2021-06-21 22:11:28 +0200

    save binary player data to database

--- updater/player/binaryData.js
+++ updater/player/binaryData.js
@@ -1,19 +1,20 @@
 const loadBinaryFHLFile = require('../../lib/filesystem/loadBinaryFHLFile')
 const generatePlayerId = require('../../lib/playerId')
 const detectSeason = require('../../lib/detectSeason')
-// const db = require('../../server/helpers/db')
+const db = require('../../server/helpers/db')
 const log = require('../../server/helpers/logger')
 
 module.exports = {
   run: async () => {
     const season = detectSeason()
-    log(`### ${season} ### STRT ### BINARY DATA ###`)
+    log(`### ${season} ### STRT ### PLAYER BINARY DATA ###`)
 
     let rawBinary = loadBinaryFHLFile('.ros')
-    let rosterId = 0
+    let simId = 0
+    const updatePlayerData = []
     const updatePlayer = []
 
-    for (let i = 0; i < 5000; i += 86) {
+    for (let i = 0; i < rawBinary.length; i += 86) {
       const data = rawBinary.slice(i, i + 86)
       const playerid = generatePlayerId(
         data.slice(0, 22).toString().substr(0, 22).trim()
@@ -22,20 +23,39 @@ module.exports = {
         data.slice(76, 79).toString().substr(0, 22).trim()
       )
       if (playerid !== '') {
-        updatePlayer.push({
+        updatePlayerData.push({
           playerid,
-          rosterId,
+          season,
+          simId
+        })
+        updatePlayer.push({
+          id: playerid,
           nation
         })
       }
-      rosterId++
-      if (rosterId >= 50) {
-        rosterId = 0
+      simId++
+      if (simId >= 50) {
+        simId = 0
       }
     }
 
-    console.log(updatePlayer)
+    if (updatePlayer.length > 0) {
+      await db('player')
+        .insert(updatePlayer)
+        .onConflict()
+        .merge(['nation'])
+        .then()
+        .catch()
+    }
+    if (updatePlayerData.length > 0) {
+      await db('playerdata')
+        .insert(updatePlayerData)
+        .onConflict()
+        .merge(['simId'])
+        .then()
+        .catch()
+    }
 
-    log(`### ${season} ### DONE ### BINARY DATA ###`)
+    log(`### ${season} ### DONE ### PLAYER BINARY DATA ###`)
   }
 }
