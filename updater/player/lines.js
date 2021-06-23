5ee4d204582e19912d8e1b46222ed8e2d6349fa3
Dominik Sander <dominik.sander@googlemail.com>
2021-06-23 22:52:36 +0200

    save lines to db

--- updater/player/lines.js
+++ updater/player/lines.js
@@ -1,7 +1,9 @@
 const loadFHLFile = require('../../lib/filesystem/loadFHLFile')
-// const generatePlayerId = require('../../lib/playerId')
+const generatePlayerId = require('../../lib/playerId')
+const { team } = require('../../lib/team')
 const detectSeason = require('../../lib/detectSeason')
-// const db = require('../../server/helpers/db')
+const detectGameday = require('../../lib/detectGameday')
+const db = require('../../server/helpers/db')
 const log = require('../../server/helpers/logger')
 
 const linesPattern = new RegExp(
@@ -22,28 +24,60 @@ module.exports = {
   run: async () => {
     const season = detectSeason()
     log(`### ${season} ### STRT ### LINES ###`)
-    const addLines = []
 
+    const gameday = detectGameday()
+
+    // get gameids for current gameday
+    const gameIds = {}
+    const games = await db('game')
+      .select('game', 'home', 'away')
+      .where({ gameday, season })
+    for (const game of games) {
+      gameIds[team(game.home)] = game.game
+      gameIds[team(game.away)] = game.game
+    }
+    const players = {}
     let rawHtml = loadFHLFile('Lines')
     const teams = rawHtml.split('<H2>')
     for (const html of teams) {
       let teamnameRegex = html.match(/>([A-Z]{1,20})</)
       if (teamnameRegex) {
+        const teamId = team(teamnameRegex[1])
         const lineTypes = html.split('<BR><BR>')
         let ltt = 0
         for (const lineType of lineTypes) {
           let row = 1
           const linesMatches = [...lineType.matchAll(linesPattern)] || []
           for (const lineData of linesMatches) {
-            console.log(`${lt[ltt]}${row}`, lineData.groups)
+            for (const pl of Object.values(lineData.groups)) {
+              if (pl && gameIds[teamId]) {
+                if (!players[generatePlayerId(pl)]) {
+                  const playerId = generatePlayerId(pl)
+                  players[playerId] = {
+                    player: playerId,
+                    season,
+                    game: gameIds[teamId],
+                    line: []
+                  }
+                }
+                players[generatePlayerId(pl)].line.push(`${lt[ltt]}${row}`)
+              }
+            }
             row++
-            addLines.push({})
           }
           ltt++
         }
-        process.exit()
       }
     }
+
+    await db('lineup')
+      .insert(
+        Object.values(players).map((p) => ({ ...p, line: p.line.join(',') }))
+      )
+      .onConflict()
+      .merge(['line'])
+      .then()
+      .catch((e) => console.log(e))
     log(`### ${season} ### DONE ### LINES ###`)
   }
 }
